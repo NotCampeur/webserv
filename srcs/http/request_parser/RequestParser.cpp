5183b7af6069657043a445198b4e4ba631511c24
Julien Maydew <MDW@MacBook-Pro-de-Julien.local>
2021-09-10 12:46:22 +0200

    ✨ using send() MSG_NISIGNAL to ignore SIGPIPE if the flag defined

--- srcs/http/request_parser/RequestParser.cpp
+++ srcs/http/request_parser/RequestParser.cpp
@@ -49,18 +49,18 @@ RequestParser::parse(void)
 			_buffer_leftovers.clear();
 			_buffer_leftovers.insert(_buffer_leftovers.begin(), &_buffer[i + 1], &_buffer[_buffer.size()]);
 
-			std::cerr << "\n### PARSED REQUEST ###\n"
-			<< "Method: " << _http_method << '\n'
-			<< "Uri (path): " << _request.uri().path << '\n'
-			<< "Uri (query): " << _request.uri().query << '\n'
-			<< "Uri (fragment): " << _request.uri().fragment << '\n'
-			<< "Http version: " << _http_version << '\n';
+			// std::cerr << "\n### PARSED REQUEST ###\n"
+			// << "Method: " << _http_method << '\n'
+			// << "Uri (path): " << _request.uri().path << '\n'
+			// << "Uri (query): " << _request.uri().query << '\n'
+			// << "Uri (fragment): " << _request.uri().fragment << '\n'
+			// << "Http version: " << _http_version << '\n';
 
-			for (std::map<std::string, std::string>::iterator it = _request.headers().begin(); it != _request.headers().end(); it++)
-			{
-				std::cerr << "Header name: " << (*it).first << '\t'
-				<< "Header value: " << (*it).second << '\n';
-			}
+			// for (std::map<std::string, std::string>::iterator it = _request.headers().begin(); it != _request.headers().end(); it++)
+			// {
+			// 	std::cerr << "Header name: " << (*it).first << '\t'
+			// 	<< "Header value: " << (*it).second << '\n';
+			// }
 			// _request.load_request_config();
 			// if (_request.method().has_body())
 			// {
